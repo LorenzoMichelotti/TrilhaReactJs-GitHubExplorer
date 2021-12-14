36fd939abe724750f3a8b0cba7594254521a8c01
lorenzo.michelotti <lorenzo.michelotti@havan.com.br>
2021-12-14 12:59:37 -0300

    listagem de repositorios

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,9 +1,13 @@
 export default function RepositoryItem (props) {
     return (
         <li>
-            <strong>{props.repository ?? 'Default'}</strong>
-            <p>Forms in React</p>
-            <a href="">Acessar repositório</a>
+            <strong>📂 {props.repository?.name ?? 'Default'}</strong>
+            <p>{props.repository?.description ?? ''}</p>
+            {props.repository?.clone_url ?
+                <a href={props.repository?.html_url}>Acessar repositório</a>
+                :
+                <legend>Repositório sem link</legend>
+            }
         </li>
     );
 }
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,14 +1,36 @@
 import RepositoryItem from "./RepositoryItem";
+import '../styles/repositories.scss';
+import { useEffect, useState } from "react";
+
+// https://api.github.com/users/LorenzoMichelotti/repos
+
+const repository = {
+    name: 'Repositorio 1',
+    description: 'react purão',
+    link: 'https://github.com/LorenzoMichelotti/TrilhaReactJs-GitHubExplorer',
+}
 
 export default function RepositoryList({}) {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/LorenzoMichelotti/repos')
+    .then( resp => resp.json() )
+    .then( data => setRepositories(data))
+  }, [])
+  
+  console.log(repositories)
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository="deuboa" description="Forms in React" link=""/>
-        <RepositoryItem/>
-        <RepositoryItem/>
+        {repositories.map((repo, key) => {
+          return (
+            <RepositoryItem key={key} repository={repo}/>
+          );
+        })}
       </ul>
     </section>
   );
